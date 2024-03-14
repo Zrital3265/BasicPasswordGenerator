b2c920242153129f9d9efe89db3c91257839bcf7
Zrital <Ztrital12580@gmail.com>
2024-03-14 11:29:42 +0545

    feat: added uppcase and lowercase

--- src/App.tsx
+++ src/App.tsx
@@ -7,29 +7,48 @@ const App: React.FC = () => {
   //for the number checkbox
   const [numbersAdd, setNumbersAdd] = useState<boolean>(true);
   //  for the character checkbox
-  const [characterAdd, setCharacterAdd] = useState<boolean>(false);
+  const [characterAdd, setCharacterAdd] = useState<boolean>(true);
+  // for lowercase
+  const [lowercase, setLowerCase] = useState<boolean>(false);
+  //for the uppercase
+  const [uppercase, setUpperCase] = useState(false);
   // for the password gemeration
   const [password, setPassword] = useState<string>("");
 
   //new hook introduced useCallback(fn,[dependencies])
   const passwordGenerator = useCallback(() => {
     let pass = "";
-    let string = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz ";
-    if (numbersAdd) string += "01234567890";
-    if (characterAdd) string += "!@$%^&*_+~`/*-";
+    let Lowercase = "abcdefghijklmnopqrstuvwxyz ";
+    let Uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let numbers = "01234567890";
+    let symbols = "~`@#$%^&*()_+/*-+";
+
+    let generatedPass = "";
+
+    if (numbersAdd) generatedPass += numbers;
+    if (characterAdd) generatedPass += symbols;
+    if (lowercase) generatedPass += Lowercase;
+    if (uppercase) generatedPass += Uppercase;
 
     for (let i = 1; i <= length; i++) {
       console.log(length);
-      let character = Math.floor(Math.random() * string.length + 1);
-      pass += string.charAt(character);
+      let character = Math.floor(Math.random() * generatedPass.length + 1);
+      pass += generatedPass.charAt(character);
     }
     setPassword(pass);
-  }, [length, numbersAdd, characterAdd, setPassword]);
+  }, [length, numbersAdd, characterAdd, setPassword, lowercase, uppercase]);
 
   //useEffect hook introduced
   useEffect(() => {
     passwordGenerator();
-  }, [length, numbersAdd, characterAdd, passwordGenerator]);
+  }, [
+    length,
+    numbersAdd,
+    characterAdd,
+    passwordGenerator,
+    lowercase,
+    uppercase,
+  ]);
 
   //useRef hook introduced
   const passwordRef = useRef<HTMLInputElement>(null);
@@ -48,13 +67,13 @@ const App: React.FC = () => {
           </span>
           <hr className="border-t border-green-400" />
         </div>
-        <div className="w-full max-w-lg px-10 py-3 mx-auto my-5 text-xl font-bold text-white bg-gray-900 rounded-xl">
+        <div className="max-w-2xl px-10 py-3 mx-auto my-5 text-xl font-bold text-white bg-gray-900 rounded-xl">
           Generate Password
-          <div className="flex mb-4 overflow-hidden shadow">
+          <div className="flex items-center justify-center mb-4 overflow-hidden shadow">
             <input
               type="text"
               value={password}
-              className="w-full px-3 py-1 my-1 font-semibold text-blue-600 rounded-lg outline-none text-md"
+              className="w-auto px-1 py-1 my-1 font-semibold text-blue-600 rounded-lg outline-none text-md"
               placeholder="Password"
               readOnly={true}
               ref={passwordRef}
@@ -85,6 +104,7 @@ const App: React.FC = () => {
                 <div className="text-lg">({length})</div>
               </label>
             </div>
+            {/* for numbers */}
             <div className="flex items-center ml-2">
               <input
                 type="checkbox"
@@ -97,6 +117,7 @@ const App: React.FC = () => {
               <label htmlFor="numberInput" className="ml-1 text-green-300">
                 Numbers
               </label>
+              {/* characters for */}
               <div className="flex items-center ml-2 ">
                 <input
                   type="checkbox"
@@ -108,6 +129,34 @@ const App: React.FC = () => {
                 ></input>
                 <label htmlFor="characterInput" className="ml-1 text-green-300">
                   Characters
+                </label>
+              </div>
+              {/* //upperCase */}
+              <div className="flex items-center ml-2 ">
+                <input
+                  type="checkbox"
+                  defaultChecked={uppercase}
+                  id="uppecaseInput"
+                  onChange={() => {
+                    setUpperCase((prev: boolean) => !prev);
+                  }}
+                ></input>
+                <label htmlFor="uppecaseInput" className="ml-1 text-green-300">
+                  upperCase
+                </label>
+              </div>
+              {/* lowercase */}
+              <div className="flex items-center ml-2 ">
+                <input
+                  type="checkbox"
+                  defaultChecked={lowercase}
+                  id="lowercaseInput"
+                  onChange={() => {
+                    setLowerCase((prev: boolean) => !prev);
+                  }}
+                ></input>
+                <label htmlFor="lowercaseInput" className="ml-1 text-green-300">
+                  lowercase
                 </label>
               </div>
             </div>
